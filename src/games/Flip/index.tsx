911e82347000199fa24a9d8294d805735d41d462
FoneFanClub <129808197+FoneFanClub@users.noreply.github.com>
2023-05-31 14:56:05 -0400

    Update index.tsx

--- src/games/Flip/index.tsx
+++ src/games/Flip/index.tsx
@@ -5,7 +5,7 @@ export default {
   name: 'Flip',
   short_name: 'flip',
   description: '',
-  creator: 'DwRFGbjKbsEhUMe5at3qWvH7i8dAJyhhwdnFoZMnLVRV',
+  creator: '9sVBzX6K4sWVHveUpWQB1gCuynev33reuNu44b4bndHY',
   image,
   theme_color: '#ad6bff',
   app: React.lazy(() => import('./App')),
